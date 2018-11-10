08b4c70a64d97209e4b7be537ecc160a1ea0f064
Sriya Lingampalli <sriyalingampalli@sriyas-mbp.attlocal.net>
2018-11-10 15:28:49 -0800

    Add and save multiple flashcards

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+struct Flashcard {
+    var question: String
+    var answer: String
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var frontLabel: UILabel!
@@ -15,9 +20,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var questionText: UITextField!
     @IBOutlet weak var answerText: UITextField!
     
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var prevButton: UIButton!
+    
+    var flashcards = [Flashcard]()
+    var currentIndex = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        readSavedFlashcards()
         // Do any additional setup after loading the view, typically from a nib.
+        if flashcards.count == 0 {
+            updateFlashcard(question: "What's the capital of Brazil?", answer: "Brasilia")
+        }
+        else {
+            updateLabels()
+            updateNextPrevButtons()
+        }
+    }
+    
+    
+    @IBAction func didTapOnPrev(_ sender: Any) {
+        currentIndex = currentIndex - 1
+        updateLabels()
+        updateNextPrevButtons()
+    }
+    @IBAction func didTapOnNext(_ sender: Any) {
+        currentIndex = currentIndex + 1
+        updateLabels()
+        updateNextPrevButtons()
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
@@ -27,7 +58,36 @@ class ViewController: UIViewController {
     }
     
     func updateFlashcard(question: String, answer: String) {
-        dismiss(animated: true)
+        let flashcard = Flashcard(question: question, answer: answer)
+        //frontLabel.text = flashcard.question
+        //backLabel.text = flashcard.answer
+        flashcards.append(flashcard)
+        
+        print("Added new flashcard")
+        print("We now have \(flashcards.count) flashcards")
+        
+        currentIndex = flashcards.count - 1
+        print("Out current index is \(currentIndex)")
+        
+        updateNextPrevButtons()
+        
+        //dismiss(animated: true)
+        updateLabels()
+    }
+    
+    func updateNextPrevButtons() {
+        if currentIndex == flashcards.count - 1 {
+            nextButton.isEnabled = false
+        }
+        else {
+            nextButton.isEnabled = true
+        }
+    }
+    
+    func updateLabels() {
+        let currentFlashcard = flashcards[currentIndex]
+        frontLabel.text = currentFlashcard.question
+        backLabel.text = currentFlashcard.answer
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -36,6 +96,20 @@ class ViewController: UIViewController {
         creationController.flashcardsController = self
     }
     
+    func saveAllFlashcardsToDisk() {
+        let dictionaryArray = flashcards.map {(card) -> [String: String] in return ["question": card.question, "answer": card.answer]
+        UserDefaults.standard.set(dictionaryArray, forKey: "flashcards")
+        print("Flashcards saved to UserDefaults")
+        }
+    }
+    
+    func readSavedFlashcards() {
+        if let dictionaryArray = UserDefaults.standard.array(forKey: "flashcards") as? [[String:String]] {
+            let savedCards = dictionaryArray.map { dictionary -> Flashcard in
+                return Flashcard(question: dictionary["question"]!, answer: dictionary["answer"]!)
+            }
+        }
+    }
 
     
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,20 @@
+## Lab 3
+
+### App Description
+Feature allows for multiple flashcards and the ability to save previously made flashcards.
+
+### App Walk-though
+<img src="http://g.recordit.co/6s3a4unOwa.gif" width=200><br>
+
+## Required
+- [x] User can browse through multiple flashcards
+- [x] User can re-open the app and see previously created flashcards
+- [x] Push code to GitHub
+## Optional
+- [ ] User can delete a flashcard
+- [ ] User can edit existing flashcard
+- [ ] User can store multiple choice questions
+
 ## Lab 2
 
 ### App Description
